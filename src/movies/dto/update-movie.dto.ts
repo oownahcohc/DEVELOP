f4a38f832706305972dbe0d4c1ac8059bb17430a
chanwoo <chocw0402@gmail.com>
2022-02-12 21:28:24 +0900

    [ADD] 데이터 전송 객체

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, IsString } from 'class-validator';
+import { CreateMovieDto } from './create-movie.dto';
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {} // PartialType은 모든 속성을 optional하게 만들어줌
