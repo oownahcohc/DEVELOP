d150db46f30c2ac34150048bf77f418f1640ec16
chanwoo <chocw0402@gmail.com>
2022-02-12 21:26:42 +0900

    [ADD] pipe 보안 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // 요청 값의 타입을 우리가 원하는 타입으로 변경해줌
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
